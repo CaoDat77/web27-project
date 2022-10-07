d2c8e21e33e46df648feb045aee8cc17754eb621
Cao Dat <tiendat772001@gmail.com>
2022-10-07 19:33:04 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    root: "./src",
+    publicDir: "assets",
+    build: {
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, "src/index.html"),
+                about: resolve(__dirname, "src/about.html"),
+                products: resolve(__dirname, "src/products.html"),
+            },
+        },
+    },
+});
